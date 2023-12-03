43d36457350330d8dd91fce933f0165993c180c6
ShohamBirman <151458916+ShohamBirman@users.noreply.github.com>
2023-12-03 13:35:25 +0200

    Update my_python_clinic_project1.py

--- my_python_clinic_project1.py
+++ my_python_clinic_project1.py
@@ -575,8 +575,11 @@ def main():
 
         st.success(f"Argument clinic session will last for {argue_time} minutes. Type 'exit' to end the argument.")
         with st.form(key='my_form'):
-            user_input = st.text_input("User:")
+            user_input_id = 0  # Initialize a variable to keep track of the user input IDs
+            user_input = st.text_input(f"User:", key=f"user_input_{user_input_id}")  # Use the unique ID for the widget
+
             submit_button = st.form_submit_button("Submit")
+
             while time.time() / 60 < end_time:
                 if submit_button:
                     responses = parse_input(user_input)
