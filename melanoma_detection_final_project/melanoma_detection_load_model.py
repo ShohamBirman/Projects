18135c501ea6f2f8fed6cdc3dd6dad9ee25fba77
Shoham Birman <151458916+ShohamBirman@users.noreply.github.com>
2024-04-18 15:39:04 +0300

    Update melanoma_detection_load_model.py

--- melanoma_detection_final_project/melanoma_detection_load_model.py
+++ melanoma_detection_final_project/melanoma_detection_load_model.py
@@ -5,7 +5,8 @@ from PIL import Image
 from model import MelanomaClassifier
 
 # Load the saved model weights
-model_path = r"C:\Users\shoha\OneDrive\מסמכים\GitHub\Projects\melanoma_detection_final_project\melanoma_classifier.pth"
+model_path = "melanoma_detection_final_project/melanoma_classifier.pth"
+#r"C:\Users\shoha\OneDrive\מסמכים\GitHub\Projects\melanoma_detection_final_project\melanoma_classifier.pth"
 
 # Load model on CPU (specify map_location=torch.device('cpu'))
 model = MelanomaClassifier()
