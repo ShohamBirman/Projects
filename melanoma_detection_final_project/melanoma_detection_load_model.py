38f06c3e5e42836e257c2a0995fafe54dff13aa8
ShohamBirman <151458916+ShohamBirman@users.noreply.github.com>
2024-04-18 16:31:26 +0300

    Update melanoma_detection_load_model.py

--- melanoma_detection_final_project/melanoma_detection_load_model.py
+++ melanoma_detection_final_project/melanoma_detection_load_model.py
@@ -16,7 +16,6 @@ model.eval()
 # Define the image transformations
 transform = transforms.Compose([
     transforms.Resize((224, 224)),
-    transforms.CenterCrop(200),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
 ])
